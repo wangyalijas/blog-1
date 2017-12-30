6ed3017f52902e0d2c1a17b39f474d16917e1992
qiutuleng <qiutuleng@gmail.com>
2017-12-30 11:30:31 +0800

    Add user test case

---
+++ tests/API/UserTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\API;
+
+use App\User;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserTest extends TestCase
+{
+    /**
+     * Test get the authenticated user
+     *
+     * @return void
+     * @throws \Exception
+     */
+    public function testAuthUser()
+    {
+        /** @var User $user */
+        $user = factory(User::class)->create();
+
+        $response = $this->auth($user)->get('/api/user');
+
+        $response
+            ->assertStatus(200)
+            ->assertExactJson($user->toArray());
+    }
+}
