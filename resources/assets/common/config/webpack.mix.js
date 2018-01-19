821b2a870feddda90b7c351b8d3d9f87f44d3070
QiuTuleng <qiutuleng@gmail.com>
2018-01-20 01:10:34 +0800

    Filter non-existent resource files

--- resources/assets/common/config/webpack.mix.js
+++ resources/assets/common/config/webpack.mix.js
@@ -1,8 +1,9 @@
+const fs = require('fs');
 
 // Append origin data to manifest before clear
 let originData = Mix.manifest.read();
 for (let filePath in originData) {
-    if (originData.hasOwnProperty(filePath)) {
+    if (originData.hasOwnProperty(filePath) && fs.existsSync(path.join(Config.publicPath, filePath))) {
         Mix.manifest.add(originData[filePath]);
     }
 }
