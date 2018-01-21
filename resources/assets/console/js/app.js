7b1ce2ac59c2dad454b47c444df3a79d7b3c6638
QiuTuleng <qiutuleng@gmail.com>
2018-01-21 11:23:03 +0800

    Add page header component

--- resources/assets/console/js/app.js
+++ resources/assets/console/js/app.js
@@ -16,6 +16,7 @@ window.Vue = require('vue');
  */
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('page-header', require('./components/PageHeader.vue'));
 
 const app = new Vue({
     el: '#app'
---
+++ resources/assets/console/js/components/PageHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <header class="page-header">
+        <div class="container-fluid">
+            <h2 class="no-margin-bottom">{{ title }}</h2>
+        </div>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: "page-header",
+        props: {
+            title: {
+                type: String,
+                required: true
+            }
+        }
+    }
+</script>
