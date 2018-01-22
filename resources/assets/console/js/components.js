581de74071ba170a08c36975f63a9d4bc47505b3
QiuTuleng <qiutuleng@gmail.com>
2018-01-22 14:06:15 +0800

    Integration Element UI to console module

--- resources/assets/console/js/components.js
+++ resources/assets/console/js/components.js
@@ -4,3 +4,10 @@ import Pagination from './components/Pagination.vue'
 
 Vue.component(PageHeader.name, PageHeader);
 Vue.component(Pagination.name, Pagination);
+
+
+// Third-party components
+import {Table, TableColumn} from 'element-ui'
+
+Vue.use(Table);
+Vue.use(TableColumn);
