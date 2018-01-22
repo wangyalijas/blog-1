ae159b2ccf7ae778e41ead8c746f07a8771e1ac3
QiuTuleng <qiutuleng@gmail.com>
2018-01-22 10:48:58 +0800

    Delete example component in console module

--- resources/assets/console/js/app.js
+++ resources/assets/console/js/app.js
@@ -15,7 +15,6 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
 Vue.component('page-header', require('./components/PageHeader.vue'));
 Vue.component('pagination', require('./components/Pagination.vue'));
 
