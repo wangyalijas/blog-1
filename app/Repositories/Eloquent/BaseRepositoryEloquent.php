682a0abef69a1eb6ec7fdd4b9600d054f3333e55
QiuTuleng <qiutuleng@gmail.com>
2018-01-26 03:07:04 +0800

    Add delete many method to base repository

--- app/Repositories/Eloquent/BaseRepositoryEloquent.php
+++ app/Repositories/Eloquent/BaseRepositoryEloquent.php
@@ -15,4 +15,18 @@ abstract class BaseRepositoryEloquent extends BaseRepository implements BaseRepo
     {
         $this->pushCriteria(app(RequestCriteria::class));
     }
+
+    /**
+     * Delete a entity in repository by id
+     *
+     * @param \Illuminate\Database\Eloquent\Collection|array $ids
+     *
+     * @return int
+     */
+    public function deleteMany($ids) {
+        if ($ids instanceof \Illuminate\Database\Eloquent\Collection) {
+            $ids = $ids->modelKeys();
+        }
+        return $this->model->whereKey($ids)->delete();
+    }
 }
--- app/Repositories/Interfaces/BaseRepositoryInterface.php
+++ app/Repositories/Interfaces/BaseRepositoryInterface.php
@@ -6,5 +6,12 @@ use Prettus\Repository\Contracts\RepositoryInterface;
 
 interface BaseRepositoryInterface extends RepositoryInterface
 {
-    //
+    /**
+     * Delete a entity in repository by id
+     *
+     * @param \Illuminate\Database\Eloquent\Collection|array $ids
+     *
+     * @return int
+     */
+    public function deleteMany($ids);
 }
