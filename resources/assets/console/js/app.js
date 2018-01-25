5fc718adf60aed913dab0f4cd99de17ef6b01f19
QiuTuleng <qiutuleng@gmail.com>
2018-01-25 14:51:13 +0800

    Add tools mixins for console module

--- resources/assets/console/js/app.js
+++ resources/assets/console/js/app.js
@@ -1,4 +1,6 @@
 import ArticlesHome from './components/articles/Home.vue'
+
+import ToolsMixin from './mixins/tools.js'
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -8,6 +10,8 @@ import ArticlesHome from './components/articles/Home.vue'
 require('./bootstrap');
 
 window.Vue = require('vue');
+
+Vue.mixin(ToolsMixin);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
---
+++ resources/assets/console/js/mixins/tools.js
@@ -0,0 +1,19 @@
+export default {
+    methods: {
+        url(path){
+            if (path && path.substring(0, 1) != '/') {
+                path = '/' + path;
+            }
+            return this.baseUrl + path;
+        },
+        redirectToUrl(url) {
+            window.location.href = url;
+        },
+        redirectToUrlFromBaseUrl(url){
+            window.location.href = this.url(url);
+        },
+        reloadPage(forcedReload = false){
+            Location.reload(forcedReload)
+        },
+    }
+}
